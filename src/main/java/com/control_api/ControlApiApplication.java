87e0660b2c95a78de88e9364a51999281814e33f
Wagner Carestini <wcarestini@gmail.com>
2026-07-21 10:35:53 -0300

    Initial structure

---
+++ src/main/java/com/control_api/ControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/control_api/ControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
