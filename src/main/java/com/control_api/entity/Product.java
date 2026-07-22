bb5e0899cffa441cdadae4d04c605c58e56ef6a5
Wagner Carestini <wcarestini@gmail.com>
2026-07-22 15:16:47 -0300

    feat: create product entity.

---
+++ src/main/java/com/control_api/entity/Product.java
@@ -0,0 +1,73 @@
+package com.control_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String name;
+    @Column(length = 500)
+    private String description;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Product() {
+        super();
+    }
+
+    public Product(final String name,
+                   final String description,
+                   final BigDecimal price,
+                   final Integer quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public void setPrice(final BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setQuantity(final Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+}
