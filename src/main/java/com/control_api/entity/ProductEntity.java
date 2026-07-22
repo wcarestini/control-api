17f55d2e4b164bebedaa66ed1e501873b457ac3a
Wagner Carestini <wcarestini@gmail.com>
2026-07-22 14:42:54 -0300

    chore: configure h2 datasource.

---
+++ src/main/java/com/control_api/entity/ProductEntity.java
@@ -0,0 +1,20 @@
+package com.control_api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public ProductEntity() {
+        super();
+    }
+}
