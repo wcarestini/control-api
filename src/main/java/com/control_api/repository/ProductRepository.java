0dafc60deb6dc45af86af4dfbfc4a6f242eaef17
Wagner Carestini <wcarestini@gmail.com>
2026-07-22 15:31:58 -0300

    feat: create productrepository.

---
+++ src/main/java/com/control_api/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.control_api.repository;
+
+import com.control_api.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
