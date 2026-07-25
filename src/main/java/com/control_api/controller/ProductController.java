44c28956f2b6d5a2ae669796411160a23bbbd3a2
Wagner Carestini <wcarestini@gmail.com>
2026-07-25 16:41:20 -0300

    feat: implement find product by id endpoint.

--- src/main/java/com/control_api/controller/ProductController.java
+++ src/main/java/com/control_api/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.control_api.dto.ProductRequest;
 import com.control_api.dto.ProductResponse;
 import com.control_api.service.CreateProductService;
 import com.control_api.service.FindAllProductsService;
+import com.control_api.service.FindProductByIdService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,11 +24,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class ProductController {
     private final CreateProductService createProductService;
     private final FindAllProductsService findAllProductsService;
+    private final FindProductByIdService findProductByIdService;
 
     public ProductController(final CreateProductService createProductService,
-                             final FindAllProductsService findAllProductsService) {
+                             final FindAllProductsService findAllProductsService,
+                             final FindProductByIdService findProductByIdService) {
         this.createProductService = createProductService;
         this.findAllProductsService = findAllProductsService;
+        this.findProductByIdService = findProductByIdService;
     }
 
     @PostMapping
@@ -48,11 +53,17 @@ public class ProductController {
                     size = 10,
                     sort = "id",
                     direction = Sort.Direction.ASC
-            )
-            final Pageable pageable
+            ) final Pageable pageable
     ) {
         var productList = findAllProductsService.findAll(pageable);
         var response = productList.map(ProductResponse::fromEntity);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> findById(@PathVariable final Long id) {
+        final var response = ProductResponse.fromEntity(findProductByIdService.findById(id));
 
         return ResponseEntity.ok(response);
     }
