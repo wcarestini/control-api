217b1df6053e27ce1ed5d43516fede5b8fc48bd4
Wagner Carestini <wcarestini@gmail.com>
2026-08-02 15:18:13 -0300

    refactor: remove unused Product constructor.

--- src/main/java/com/control_api/entity/Product.java
+++ src/main/java/com/control_api/entity/Product.java
@@ -39,18 +39,6 @@ public class Product {
         this.quantity = quantity;
     }
 
-    public Product(final Long id,
-                   final String name,
-                   final String description,
-                   final BigDecimal price,
-                   final Integer quantity) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
     public void update(final String name,
                        final String description,
                        final BigDecimal price,
